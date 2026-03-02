d4443963fe3134af9554775792e8287c12d429f4
Sagarnag-Jesus <23308060610335@cetis61.edu.mx>
2026-03-02 11:50:19 -0500

    aver

---
+++ main.py
@@ -0,0 +1,44 @@
+import flet as ft
+
+def main(page: ft.Page):
+    
+    page.add(ft.Text(
+        value="Crear un evento",
+        size=60,
+        weight=ft.FontWeight.BOLD,
+        italic=True,
+        text_align=ft.TextAlign.CENTER,
+        max_lines=2,
+        overflow=ft.TextOverflow.ELLIPSIS
+    ))
+    
+    page.add(ft.TextField(
+        label="Nombre del evento",
+        hint_text="Ingrese el nombre del evento",
+        width=300,
+        max_length=50,
+    ))
+    
+    page.add(ft.Dropdown(
+        options=[
+            ft.DropdownOption("Conferencia"),
+            ft.DropdownOption("Taller"),
+            ft.DropdownOption("Reunión"),
+        ]
+    ))
+    
+    page.add(ft.Checkbox(
+        label="¿Requiere inscripción previa?"
+    ))
+    
+    page.add(ft.Slider(
+        min=1,
+        max=8,
+        divisions=8,
+        value=1,
+        label="Horas: {value}",
+        height=25
+))
+    
+
+ft.run(main)
